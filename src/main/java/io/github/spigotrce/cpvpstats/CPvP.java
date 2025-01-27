93e0869a3faa524c7e749186e5dca5cba5ae95e4
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-01-27 12:51:27 +0530

    feat: boot

---
+++ src/main/java/io/github/spigotrce/cpvpstats/CPvP.java
@@ -0,0 +1,29 @@
+package io.github.spigotrce.cpvpstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class CPvP extends JavaPlugin {
+    public static CPvP INSTANCE;
+    public static Logger LOGGER;
+
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+        LOGGER = getLogger();
+        LOGGER.info("Booting CPvP Stats...");
+        LOGGER.info("--------------------------------");
+        LOGGER.info("Written by SpigotRCE");
+        LOGGER.info("--------------------------------");
+        LOGGER.info("Thanks for using this plugin!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        LOGGER.info("CPvP Stats has been disabled.");
+        LOGGER.info("Good bye!");
+        LOGGER.info("We hope to meet you once again!");
+    }
+}
